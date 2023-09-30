320850d11161ab7ead05dd3ad16e95d034c0d901
Olaf Willocx <olaf@usa.com>
2023-09-30 17:25:38 +0200

    style in 14

--- 14/14.cc
+++ 14/14.cc
@@ -8,8 +8,7 @@ int main()
     /* maximum length number */ size_t m = 1,
     /* maximum length */ ml = ls[m];
     for (size_t i = 1; i < 1e6; i++)
-    {   size_t l = 0, j = i;
-        for (;; l++)
+    {   for (size_t l = 0, j = i;; l++)
         {   if (ls.contains(j))
             {   l += ls[j];
                 ls[i] = l;
