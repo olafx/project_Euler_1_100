75550fbb66b2e5404e5d57204ab7f7b03f4472c4
Olaf Willocx <olaf@usa.com>
2023-09-30 16:12:38 +0200

    14

---
+++ 14/14.cc
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <cstdio>
+#include <unordered_map>
+
+int main()
+{
+    /* chain lengths */ std::unordered_map<size_t, size_t> ls {{1, 1}};
+    /* maximum length number */ size_t m = 1,
+    /* maximum length */ ml = ls[m];
+    for (size_t i = 1; i < 1e6; i++)
+    {   size_t l = 0, j = i;
+        for (;; l++)
+        {   if (ls.contains(j))
+            {   l += ls[j];
+                ls[i] = l;
+                if (l > ml)
+                {   m = i;
+                    ml = l;
+                }
+                break;
+            }
+            if (j % 2 == 0)
+                j /= 2;
+            else
+                j = 3*j+1;
+        }
+    }
+    printf("%zu\n", m);
+}
